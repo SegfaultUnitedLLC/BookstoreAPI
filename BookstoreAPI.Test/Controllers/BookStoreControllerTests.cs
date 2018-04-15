f9244d6b22e036da20567e8712ba455ac283962e
Hank Lin <hanklin@buffalo.edu>
2018-04-15 18:48:42 -0400

    test(unit): added tests for test driven development
    :memo: Added setup and framework to begin TDD

---
+++ BookstoreAPI.Test/Controllers/BookStoreControllerTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookstoreAPI.Test.Controllers
+{
+    [TestFixture]
+    class BookStoreControllerTests
+    {
+        [Test]
+        public void BookStoreApiTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
